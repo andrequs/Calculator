11aae7687030373b5d1ddcc1577ca30f82a5b041
Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-06-12 21:21:35 +0300

    Updated manifest

--- app/src/main/java/com/example/admin/nyproject/MainActivity.java
+++ app/src/main/java/com/example/admin/nyproject/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spec = new ArrayList<>();
