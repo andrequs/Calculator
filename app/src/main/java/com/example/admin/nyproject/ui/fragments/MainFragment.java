d790b4feb9819f8a58684963e5b18b153c79407b
Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-06-12 21:37:30 +0300

    Added child fragments

--- app/src/main/java/com/example/admin/nyproject/ui/fragments/MainFragment.java
+++ app/src/main/java/com/example/admin/nyproject/ui/fragments/MainFragment.java
@@ -1,6 +1,22 @@
 package com.example.admin.nyproject.ui.fragments;
 
+import androidx.annotation.LayoutRes;
+
+import com.example.admin.nyproject.R;
 import com.example.admin.nyproject.core.BaseFragment;
 
 public class MainFragment extends BaseFragment {
+
+    //region BaseFragment
+    @LayoutRes
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+    //endregion
 }
--- app/src/main/java/com/example/admin/nyproject/ui/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/admin/nyproject/ui/fragments/SettingsFragment.java
@@ -1,6 +1,22 @@
 package com.example.admin.nyproject.ui.fragments;
 
+import androidx.annotation.LayoutRes;
+
+import com.example.admin.nyproject.R;
 import com.example.admin.nyproject.core.BaseFragment;
 
 public class SettingsFragment extends BaseFragment {
+
+    //region BaseFragment
+    @LayoutRes
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_settings;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+    //endregion
 }
